999e77e4c75a99aae6863a8bb64ccdca1b5d4ca8
Mamady Sillah <supurpisces310@msn.com>
2020-05-14 19:45:41 -0400

    created constructor super for fetch i will do for json data for country statistics

---
+++ src/Components/PieChart.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Pie } from 'react-chartjs-2';  
+
+class PieChart extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
+
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default PieChart;
